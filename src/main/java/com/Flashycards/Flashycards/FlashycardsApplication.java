40c8fc60ccf36da1a115c7e01eafb9daedf5336a
Chungsing Arguello <chung@Chungsings-iMac.local>
2020-02-13 17:11:46 -0500

    first commit

---
+++ src/main/java/com/Flashycards/Flashycards/FlashycardsApplication.java
@@ -0,0 +1,13 @@
+package com.Flashycards.Flashycards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashycardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashycardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Flashycards/Flashycards/FlashycardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Flashycards.Flashycards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashycardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
